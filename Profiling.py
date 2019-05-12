705a03b84f7bab7be5bafbeb5a6d9ee17625e1fb
Will <w255li@edu.uwaterloo.ca>
2019-05-12 16:24:11 -0400

    Added some simple graphs and tables

--- Profiling.py
+++ Profiling.py
@@ -3,21 +3,33 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-import matplotlib.pyplot as plt
 
+#import data
 df = pd.read_csv('Workbook5Clean.csv')
 
+#turn certain columns into categories
 df['Class'] = df['Class'].astype('category')
 df['Pos'] = df['Pos'].astype('category')
-df['Drafted'] = df['Pos'].astype('category')
+df['Drafted'] = df['Drafted'].astype('category')
 
+#subset data into positon and drafted
+df_draftedByPosition = df[['Pos', 'Drafted']]
+x = df_draftedByPosition.Pos
+y = df_draftedByPosition.Drafted
+table_draftedByPos = pd.crosstab(x,y, rownames = ['x'], colnames = ['y'])
+print(table_draftedByPos)
+
+#correlation between points and drafted (very trivial)
+plt.scatter(df['FG'],df['FGA'])
+
+
+# find correlations between numerical values
 corr = df.corr()
-print(corr)
 
-#corrPlot = plt.matshow(df.corr())
-#corrPlot.set_xticklabels(list(df.columns.values))
+#get column names
 names = list(df.columns.values)
 
+#show corrplot
 fig = plt.figure()
 ax = fig.add_subplot(111)
 cax = ax.matshow(corr, vmin=-1, vmax=1)
